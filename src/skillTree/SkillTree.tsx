2703e5604e2f72f0ae066d1dc0961adddb5e9d9e
NikIsImba <NiklIsImba@nik123691@gmail.com>
2024-03-22 11:18:25 +0100

    Fixed Draggable StrictDom Error

--- src/skillTree/SkillTree.tsx
+++ src/skillTree/SkillTree.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Group from './Group';
 import Draggable from 'react-draggable';
 
@@ -10,17 +10,30 @@ interface SkillTreeProps {
 
 const SkillTree: React.FC<SkillTreeProps> = ({ baseSize, groupLocations }) => {
   const [scale, setScale] = useState(1);
+  const divRef = useRef<HTMLDivElement>(null);
+  const draggableRef = React.useRef(null);
 
-  const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+  const handleWheel = (e: WheelEvent) => {
     e.preventDefault();
     const scaleChange = e.deltaY < 0 ? 1.1 : 0.9;
     setScale(prevScale => prevScale * scaleChange);
   }
 
+  useEffect(() => {
+    const div = divRef.current;
+    if (div) {
+      div.addEventListener('wheel', handleWheel, { passive: false });
+      return () => {
+        div.removeEventListener('wheel', handleWheel);
+      };
+    }
+  }, []);
+
   return (
-    <div onWheel={handleWheel} style={{ transform: `scale(${scale})`, transformOrigin: '0 0' }}>
-      <Draggable scale={scale}>
+    <div ref={divRef} style={{ transform: `scale(${scale})`, transformOrigin: '0 0' }}>
+      <Draggable scale={scale} nodeRef={draggableRef}>
         <div
+          ref={draggableRef}
           className='bg-yellow-950 absolute'
           style={{ width: baseSize[0], height: baseSize[1] }}
         >
