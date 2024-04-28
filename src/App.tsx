04e4204273b652d7f553759b6c35e20da11a42ed
Niklas <nik123691@gmail.com>
2024-04-28 20:54:02 +0200

    Prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Stage, Container, Sprite, Text } from '@pixi/react';
 
 function App() {
   return (
-    <div className="bg-yellow-800">
-      <div className="p-10">
+    <div className='bg-yellow-800'>
+      <div className='p-10'>
         <MyComponent />
       </div>
     </div>
@@ -16,14 +16,14 @@ export const MyComponent = () => {
   return (
     <Stage width={600} height={1222} options={{ background: 0xffffff }}>
       <Sprite
-        image="https://pixijs.io/pixi-react/img/bunny.png"
+        image='https://pixijs.io/pixi-react/img/bunny.png'
         x={400}
         y={200}
         anchor={{ x: 0.5, y: 0.5 }}
       />
 
       <Container x={400} y={330}>
-        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
+        <Text text='Hello World' anchor={{ x: 0.5, y: 0.5 }} />
       </Container>
     </Stage>
   );
